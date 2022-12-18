d784a264f078012b64056b7f4dcf255635ce3231
KhanhLong_Truong <119829474+KhanhLong-PKA@users.noreply.github.com>
2022-12-18 16:40:42 +0700

    Code cua Minh Quang

--- MinhQuang.cpp
+++ MinhQuang.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
+
 using namespace std;
+
 void NhapMang(int *a,int n)
 {
     cout << "Nhap phan tu: ";
     for(int i=0; i<n; i++)
     {
         cin >> a[i];
-    }
-}
-void XuatMang(int *a, int &n)
-{
-    cout << "\nXuat mang: ";
-    for (int i = 0; i < n; i++)
-    {
-        cout << a[i] << " ";
     }
 }
 
@@ -23,15 +17,15 @@ void Xoaphantu(int *&a, int &n, int &y)
     {
         a[x] = a[x+1];
     }
-
+    
     int *temp = new int[n-1];
     for(int i = 0; i < n-1; i++)
     {
         temp[i] = a[i];
     }
-
+    
     delete[] a;
-
+    
     a = new int[n-1];
     for (int i = 0; i < n-1; i++)
     {
@@ -45,64 +39,62 @@ void Do(int *&a,int &n, int &dem)
 {
     for (int y = 0; y < n; y++)
     {
-        if (a[y] > 9)
-        {
-            break;
-        }
-        else if(a[0] == a[n-1])
+        
+        if(a[0] == a[n-1])
         {
             Xoaphantu(a, n, y);
             n--;
-
+            
             Do(a, n, dem);
         }
         else
         {
             for (int i = 0; i < n-1; i++)
             {
-                if (a[i] > 9)
-                {
-                    break;
-                }
+                
                 for (int j = i+1; j < n; j++)
                 {
-                    if (a[j] > 9)
+                    
+                    if (a[i] == a[j])
                     {
-                        break;
-                    }
-                    else if (a[i] == a[j])
-                    {
-
+                        
                         Xoaphantu(a, n, i);
                         n--;
                         dem++;
 
                         Do(a, n, dem);
-
+                        
                     }
-
+                    
                 }
-
+                    
             }
         }
     }
-
+    
 }
 int main()
 {
     int n;
-
     int dem = 0;
     cout << "Nhap so luong phan tu: ";
     cin >> n;
     int *a = new int[n];
     NhapMang(a, n);
-
-    Do(a, n, dem);
-    cout << dem+n;
-
+    if(n == 0)
+    {
+        cout << 0;
+    }
+    else if(n == 1)
+    {
+        cout << 1;
+    }
+    else
+    {
+        Do(a, n, dem);
+        cout << dem+n;
+    }
     delete[] a;
     system("pause");
     return 0;
 }
-
